ae493bf73bb7145e43dc78bc4aa27aa662d7bf9a
abol <itgholamollah@gmail.com>
2022-04-22 00:38:37 +0430

    clean code

--- script.js
+++ script.js
@@ -6,32 +6,32 @@ const input_search = document.getElementById("input_search");
 const main = document.getElementsByTagName("main")[0];
 
 
-input_search.addEventListener("input",()=>{
-    if(input_search.value=="")return;
-    if(/^[ضصثقفغعهخحجچشسیبلاتنمکگظطزرذدپو.]/.test(input_search.values))
+input_search.addEventListener("input", () => {
+    if (input_search.value == "") return;
+    if (/^[ضصثقفغعهخحجچشسیبلاتنمکگظطزرذدپو.]/.test(input_search.values))
         document.body.className = "direction_rtl"
     else
         document.body.className = "direction_ltr"
 })
 
-input_search.addEventListener("keyup",ev=>{
-    if(ev.key == "Enter" && input_search.value.length>0)
+input_search.addEventListener("keyup", ev => {
+    if (ev.key == "Enter" && input_search.value.length > 0)
         search(input_search.value);
 })
 
-function search(word){
-    fetch("https://api.dictionaryapi.dev/api/v2/entries/en/"+word)
-    .then(j=>j.json())
-    .then(arr=>{
-        console.log(arr)
-        let word = arr[0]['word']
-        let phonetic =  arr[0]['phonetics'][0]['text'] || arr[0]['phonetics'][1]['text']
-        let partOfSpeech = arr[0]['meanings'][0]['partOfSpeech']
-        let definition = arr[0]['meanings'][0]['definitions'][0]['definition']
-        console.log(word,phonetic,partOfSpeech,definition)
-        main.innerHTML = "";
+function search(word) {
+    fetch("https://api.dictionaryapi.dev/api/v2/entries/en/" + word)
+        .then(j => j.json())
+        .then(arr => {
+            console.log(arr)
+            let word = arr[0]['word']
+            let phonetic = arr[0]['phonetics'][0]['text'] || arr[0]['phonetics'][1]['text']
+            let partOfSpeech = arr[0]['meanings'][0]['partOfSpeech']
+            let definition = arr[0]['meanings'][0]['definitions'][0]['definition']
+            console.log(word, phonetic, partOfSpeech, definition)
+            main.innerHTML = "";
 
-        main.innerHTML = `
+            main.innerHTML = `
         <section>
             <h2 class="word">${word}</h2>
             <h3 class="phonetic">${phonetic}</h3>
@@ -39,13 +39,13 @@ function search(word){
             <p class="definition">${definition}</p>
         </section>
         `;
-    })
-    .catch(e=>{
-        main.innerHTML = `
+        })
+        .catch(e => {
+            main.innerHTML = `
         <section>
             <h1 class="no_match">sorr:( , no match !</h1>
         </section>
-        `;       
-    });
+        `;
+        });
 
 }
